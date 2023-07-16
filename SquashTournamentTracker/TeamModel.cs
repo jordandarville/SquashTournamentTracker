86766fe959f1ac19ea069259c9efa7ea73642634
Jordan Darville <jrdarville@gmail.com>
2023-07-16 10:04:57 -0400

    Add project files.

---
+++ SquashTournamentTracker/TeamModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquashTournamentTracker
+{
+    internal class TeamModel
+    {
+        public List<Person> TeamMembers { get; set; } = new List<Person>;
+    }
+}
